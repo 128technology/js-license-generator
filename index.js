2d58d5e6c9cdbc4dca32a43e32e91c06b0dd25ba
Benjamin Beshel <bbeshel@128technology.com>
2019-01-15 16:55:12 -0500

    Converting module to use license-checker in place of nlf. Made necessary changes to index.js and builders.js maintain functionality with the new package. Added a --readme flag that allows us to explicitly specify if we will allow README.md files in place of missing LICENSE files Added a check against URLs that commonly point to package LICENSE files so that we can include the online link in the JSON output.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const minimist = require('minimist');
 const _ = require('lodash');
 const chalk = require('chalk');
+const lc = require('license-checker');
 
 const { TextBuilder, JSONBuilder } = require('./lib/builders');
 const { exit } = require('./lib/util');
@@ -24,11 +25,13 @@ if (argv.help) {
   console.log('--cacheFile=<path>       An absolute path to a JSON cache file for looking up missing licenses.');
   console.log('--ignore=<name1, name2>  Comma separated package names to ignore.');
   console.log('--production             Search for licenses on production dependencies. Default is development.');
+  console.log('--readme                 Use the readme as a fallback when LICENSE is not available.');
   console.log('--help                   This help.');
   process.exit(0);
 }
 
 const production = !!argv.production;
+const readme = !!argv.readme;
 const { directory, cacheFile, ignore } = argv;
 
 const ignoreSet = ignore ? new Set(ignore.split(',')) : new Set();
@@ -36,25 +39,40 @@ const cache = cacheFile ? JSON.parse(fs.readFileSync(cacheFile, 'utf-8')) : null
 
 console.log(`Searching for ${production ? 'production' : 'development'} licenses in ${directory}`);
 
-nlf.find({ directory, production }, (err, npmPackages) => {
+let promises = [];
+
+lc.init({ start: directory, production }, (err, npmPackages) => {
   if (err) {
     exit(err);
   }
 
   const builders = [new TextBuilder(), new JSONBuilder()];
 
-  npmPackages.forEach(npmPackage => {
-    const { name, version } = npmPackage;
+  _.forEach(npmPackages, (npmPackage, nameVer) => {
+    const [name, version] = [_.initial(nameVer.split('@')).join('@'), _.last(nameVer.split('@'))];
 
+    npmPackage.name = npmPackage.name ? npmPackage.name : name;
+    npmPackage.version = npmPackage.version ? npmPackage.version : version;
     if (ignoreSet.has(name)) {
       return;
     }
 
-    const packageID = TextBuilder.getPackageIdentifier(npmPackage);
-    const sources = _.get(npmPackage, 'licenseSources.license.sources', []);
+    const packageID = nameVer;
+    const licenseFile = _.get(npmPackage, 'licenseFile', '');
 
-    if (sources.length > 0) {
+    let hasReadme = false;
+    if (!readme) {
+      hasReadme = licenseFile.toLowerCase().indexOf('readme') > -1;
+      if (hasReadme) {
+        npmPackage.licenseFile = '';
+      }
+    }
+
+    const text = licenseFile.length > 0 && (readme || !hasReadme) ? fs.readFileSync(licenseFile, 'utf-8') : null;
+
+    if (licenseFile.length > 0 && text) {
       builders.forEach(builder => {
+        npmPackage.text = text;
         builder.add(npmPackage);
       });
     } else if (cache && _.get(cache, [name, version], null)) {
@@ -73,18 +91,16 @@ nlf.find({ directory, production }, (err, npmPackages) => {
       builders.forEach(builder => {
         builder.addCached(name, version, lastVersionDefinition);
       });
-    } else if (_.get(npmPackage, 'licenseSources.package.sources', []).length > 0) {
-      const packageSources = _.get(npmPackage, 'licenseSources.package.sources', [])
-        .map(({ license }) => license)
-        .join(', ');
+    } else if (_.get(npmPackage, 'license', '').length > 0) {
+      const packagelicenseFile = `${_.get(npmPackage, 'license')}`;
 
       console.log(
-        chalk.red(`License source not found for ${packageID}, derived license types are: ${packageSources}.`)
+        chalk.red(`License source not found for ${packageID}, derived license types are: ${packagelicenseFile}.`)
       );
 
       builders.forEach(builder => {
         builder.addEmpty(name, version);
-      });
+      }); 
     } else {
       console.log(chalk.red(`License could not be detected for package ${packageID}.`));
 
@@ -92,9 +108,12 @@ nlf.find({ directory, production }, (err, npmPackages) => {
         builder.addEmpty(name, version);
       });
     }
+    promises.push(builders[1].buildLink(name, npmPackage.repository, npmPackage.licenseFile));
   });
-
-  builders.forEach(builder => {
-    builder.write();
+  
+  Promise.all(promises).then(() => {
+    builders.forEach(builder => {
+      builder.write();
+    });
   });
 });
