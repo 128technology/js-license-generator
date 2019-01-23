fff8ac24a084e67ccafedb14302ac6404b8e01b7
Benjamin Beshel <bbeshel@128technology.com>
2019-01-23 11:46:42 -0500

    Changing prettier settings, formatting files to match

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
-const minimist = require("minimist");
-const _ = require("lodash");
-const chalk = require("chalk");
-const lc = require("license-checker");
+const fs = require('fs');
+const minimist = require('minimist');
+const _ = require('lodash');
+const chalk = require('chalk');
+const lc = require('license-checker');
 
-const { TextBuilder, JSONBuilder, XLSXBuilder } = require("./lib/builders");
-const { exit } = require("./lib/util");
+const { TextBuilder, JSONBuilder, XLSXBuilder } = require('./lib/builders');
+const { exit } = require('./lib/util');
 
 const argv = minimist(process.argv, {
   default: {
@@ -16,27 +16,19 @@ const argv = minimist(process.argv, {
 });
 
 if (argv.help) {
-  console.log("Usage:");
-  console.log("license-generator [arguments]");
+  console.log('Usage:');
+  console.log('license-generator [arguments]');
   console.log();
-  console.log("Arguments:");
-  console.log("--directory=<dir>        The directory to search.");
+  console.log('Arguments:');
+  console.log('--directory=<dir>        The directory to search.');
+  console.log('--cacheFile=<path>       An absolute path to a JSON cache file for looking up missing licenses.');
+  console.log('--ignore=<name1, name2>  Comma separated package names to ignore.');
+  console.log('--production             Search for licenses on production dependencies. Default is development.');
   console.log(
-    "--cacheFile=<path>       An absolute path to a JSON cache file for looking up missing licenses."
+    '--links                  Check online for a reference of the license from the default github location and add it to the package output.'
   );
-  console.log(
-    "--ignore=<name1, name2>  Comma separated package names to ignore."
-  );
-  console.log(
-    "--production             Search for licenses on production dependencies. Default is development."
-  );
-  console.log(
-    "--links                  Check online for a reference of the license from the default github location and add it to the package output."
-  );
-  console.log(
-    "--readme                 Use the readme as a fallback when LICENSE is not available."
-  );
-  console.log("--help                   This help.");
+  console.log('--readme                 Use the readme as a fallback when LICENSE is not available.');
+  console.log('--help                   This help.');
   process.exit(0);
 }
 
@@ -45,16 +37,10 @@ const readme = !!argv.readme;
 const links = !!argv.links;
 const { directory, cacheFile, ignore } = argv;
 
-const ignoreSet = ignore ? new Set(ignore.split(",")) : new Set();
-const cache = cacheFile
-  ? JSON.parse(fs.readFileSync(cacheFile, "utf-8"))
-  : null;
+const ignoreSet = ignore ? new Set(ignore.split(',')) : new Set();
+const cache = cacheFile ? JSON.parse(fs.readFileSync(cacheFile, 'utf-8')) : null;
 
-console.log(
-  `Searching for ${
-    production ? "production" : "development"
-  } licenses in ${directory}`
-);
+console.log(`Searching for ${production ? 'production' : 'development'} licenses in ${directory}`);
 
 let promises = [];
 
@@ -67,8 +53,8 @@ lc.init({ start: directory, production }, (err, npmPackages) => {
   const builders = [new TextBuilder(), jsonBuilder];
 
   _.forEach(npmPackages, (npmPackage, nameVer) => {
-    const name = _.initial(nameVer.split("@")).join("@");
-    const version = _.last(nameVer.split("@"));
+    const name = _.initial(nameVer.split('@')).join('@');
+    const version = _.last(nameVer.split('@'));
 
     npmPackage.name = npmPackage.name ? npmPackage.name : name;
     npmPackage.version = npmPackage.version ? npmPackage.version : version;
@@ -77,20 +63,17 @@ lc.init({ start: directory, production }, (err, npmPackages) => {
     }
 
     const packageID = nameVer;
-    const licenseFile = npmPackage.licenseFile || "";
+    const licenseFile = npmPackage.licenseFile || '';
 
     let hasReadme = false;
     if (!readme) {
-      hasReadme = licenseFile.toLowerCase().indexOf("readme") > -1;
+      hasReadme = licenseFile.toLowerCase().indexOf('readme') > -1;
       if (hasReadme) {
-        npmPackage.licenseFile = "";
+        npmPackage.licenseFile = '';
       }
     }
 
-    const text =
-      licenseFile.length > 0 && (readme || !hasReadme)
-        ? fs.readFileSync(licenseFile, "utf-8")
-        : null;
+    const text = licenseFile.length > 0 && (readme || !hasReadme) ? fs.readFileSync(licenseFile, 'utf-8') : null;
 
     if (licenseFile.length > 0 && text) {
       builders.forEach(builder => {
@@ -106,48 +89,32 @@ lc.init({ start: directory, production }, (err, npmPackages) => {
         builder.addCached(name, version, cachedDefinition);
       });
     } else if (cache && _.get(cache, [name], null)) {
-      console.log(
-        chalk.yellow(
-          `License found in cache for a different version of ${packageID}.`
-        )
-      );
+      console.log(chalk.yellow(`License found in cache for a different version of ${packageID}.`));
 
-      const lastVersionDefinition = _.last(
-        _.values(_.get(cache, [name], null))
-      );
+      const lastVersionDefinition = _.last(_.values(_.get(cache, [name], null)));
 
       builders.forEach(builder => {
         builder.addCached(name, version, lastVersionDefinition);
       });
-    } else if (_.get(npmPackage, "license", "").length > 0) {
-      const packagelicenseFile = `${_.get(npmPackage, "license")}`;
+    } else if (_.get(npmPackage, 'license', '').length > 0) {
+      const packagelicenseFile = `${_.get(npmPackage, 'license')}`;
 
       console.log(
-        chalk.red(
-          `License source not found for ${packageID}, derived license types are: ${packagelicenseFile}.`
-        )
+        chalk.red(`License source not found for ${packageID}, derived license types are: ${packagelicenseFile}.`)
       );
 
       builders.forEach(builder => {
         builder.addEmpty(name, version);
       });
     } else {
-      console.log(
-        chalk.red(`License could not be detected for package ${packageID}.`)
-      );
+      console.log(chalk.red(`License could not be detected for package ${packageID}.`));
 
       builders.forEach(builder => {
         builder.addEmpty(name, version);
       });
     }
     if (links) {
-      promises.push(
-        jsonBuilder.buildLink(
-          name,
-          npmPackage.repository,
-          npmPackage.licenseFile
-        )
-      );
+      promises.push(jsonBuilder.buildLink(name, npmPackage.repository, npmPackage.licenseFile));
     }
   });
 
